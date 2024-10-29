2b2d2bce7947b4dcfc92e97559d1d75bb7626e0a
grrowerr <grouw.uzkiy@yandex.ru>
2024-10-29 18:58:55 +0300

    POJO class Cat -> created bean cat -> created bean cat -> prototype app -> done tests -> done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        bean.setMessage("Hello World2");
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+        System.out.println(catBean.getAge());
     }
 }
